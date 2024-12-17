80fc931e8c15d9a3d31cc04241afddea3befef79
Taras <taraslebedunsky@gmail.com>
2024-12-18 01:42:42 +0200

    Trust Section

--- src/js/script.js
+++ src/js/script.js
@@ -161,7 +161,33 @@ $(document).ready(function () {
         arrows: true,
         dots: true,
         slidesToShow: 3,
-
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
 
