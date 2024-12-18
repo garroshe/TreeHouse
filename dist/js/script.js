d572638031d6b3d420458d674c2f7230f097a2fd
Taras <taraslebedunsky@gmail.com>
2024-12-19 01:55:39 +0200

    Adapted Slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -161,27 +161,51 @@ $(document).ready(function () {
         arrows: true,
         dots: true,
         slidesToShow: 3,
-        autoplay: true,
+        // autoplay: true,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 600,
+                breakpoint: 1224,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1,
+                    centerMode: true,
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: '10px',
+
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerMode: true,
+
+                }
+            },
+            {
+                breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+
+
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+
                 }
             }
         ]
